fbaf5a5f00d100ac4169b5eee74428d4b068d634
Anders Jensen <anders.c.jensen@gmail.com>
2018-11-24 18:45:08 +0100

    Add web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// from actix.rs/docs/getting-started/
+
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+   "Canary is alive"
+}
+
 fn main() {
-    println!("Canary is alive");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+      .bind("127.0.0.1:8088")
+      .unwrap()
+      .run();
 }

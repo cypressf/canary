1e04a11bd39eb2e04668147ba9909a1077ccddf5
Anders Jensen <anders.c.jensen@gmail.com>
2019-01-19 21:13:05 +0100

    Changes port binding behavior to take ENV var from Docker file.

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM alpine:latest
 
 COPY ./target/x86_64-unknown-linux-musl/release/canary .
 
+# Configure and document the service HTTP port.
+ENV PORT 8088
+EXPOSE $PORT
+
 ENTRYPOINT ["./canary"]
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use actix_web::{server, App, HttpRequest};
+use std::env;
 
 fn index(_req: &HttpRequest) -> &'static str {
     "Canary is alive"
 }
 
+/// utility function from the heroku buildpack example project
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080)
+}
+
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("0.0.0.0:8088")
-        .expect("Can not bind to port 8088")
+    use std::net::{SocketAddr};
+ //   let sys = actix::System::new("updater");
+    let addr = SocketAddr::from(([0, 0, 0, 0], get_server_port()));
+println!("Starting server.");
+
+      server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind(addr)
+        .expect("Can not bind to port X")
         .run();
+
 }
